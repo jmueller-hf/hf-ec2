3059d64a646eae5087a163c60a8fa1ee0b684689
Jason Mueller <jmueller@healthfirst.org>
2023-03-01 17:57:15 -0500

    Updated template

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
-  value       = aws_instance.instance.id
+  value       = module.ec2.instance_id
   description = "The instance id"
 }
 
 output "instance_ip" {
-  value       = aws_instance.instance.private_ip
+  value       = module.ec2.instance_ip
   description = "The instance private IPv4 address"
 }
 
 output "instance_name" {
-  value       = local.instance_name
+  value       = module.ec2.instance_name
   description = "The instance name"
 }
